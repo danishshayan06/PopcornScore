c339f79526d6389079fbd071e2b0c03a914cc879
Danish Shayan <danishshayan06@gmail.com>
2026-04-25 16:40:02 +0500

    Fresh start from clean local folder

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import PopcornScore from "./PopcornScore";
+
+function App() {
+   return (
+      <PopcornScore />
+   )
+}
+
+export default App
